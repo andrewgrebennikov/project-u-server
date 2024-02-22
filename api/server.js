032eb526c325e3747bdd00a2c9b7f9768b536e85
Andrey Grebennikov <lostplanet08@mail.ru>
2024-02-22 11:13:55 +0400

    1. Add vercel.json

--- api/server.js
+++ api/server.js
@@ -1,31 +1,46 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
 
-const middlewares = jsonServer.defaults()
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const { users = [] } = db;
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+        const userFromBd = users.find((user) => user.username === username && user.password === password);
 
-// Export the Server API
-module.exports = server
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
