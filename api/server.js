0d152a0d98ce3a6b380fd428491adedb5569b8fe
Andrey Grebennikov <lostplanet08@mail.ru>
2024-02-22 11:29:04 +0400

    1. Add vercel.json

--- api/server.js
+++ api/server.js
@@ -14,9 +14,29 @@ const router = jsonServer.router(db);
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults();
+const middlewares = [jsonServer.defaults(), jsonServer.bodyParser];
 
 server.use(middlewares)
+
+server.post('/login', (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const {users = []} = db;
+
+        const userFromBd = users.find((user) => user.username === username && user.password === password);
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({message: 'User not found'});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({message: e.message});
+    }
+});
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
