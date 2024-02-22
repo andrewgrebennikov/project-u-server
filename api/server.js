707da0af216cb1a4753515cc89719d03a7a5189c
Andrey Grebennikov <lostplanet08@mail.ru>
2024-02-22 12:24:58 +0400

    1. Fix path

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ server.use(middlewares)
 server.post('/login', (req, res) => {
     try {
         const {username, password} = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'db.json'), 'UTF-8'));
         const {users = []} = db;
 
         const userFromBd = users.find((user) => user.username === username && user.password === password);
@@ -39,7 +39,7 @@ server.post('/login', (req, res) => {
 
 server.use((req, res, next) => {
     if (!req.headers.authorization) {
-        return res.status(403).json({ message: 'AUTH ERROR' });
+        return res.status(403).json({message: 'AUTH ERROR'});
     }
 
     next();
